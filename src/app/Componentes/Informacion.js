ec9b95915d816a8f0ed844c8cc43d214e40fae46
Duvan Mancilla <Duvanred0@gmail.com>
2024-01-31 15:27:47 -0500

    Uso de Next/Image en todas las imagenes y cambios en el texto generado

--- src/app/Componentes/Informacion.js
+++ src/app/Componentes/Informacion.js
@@ -1,18 +1,23 @@
+import Pokemon from '../../../public/Images/hero-imagen.png'
+import Image from 'next/image'
+
 export default function Informacion() {
     return(
     <>
-    <div className="flex justify-center items-center flex-col gap-4 m-10" id="Que-es" >
-    <article className="flex flex-col gap-3 max-w-[900px]" >
-    <h3 className="text-xl font-semibold">¿QUE ES POKEMON?</h3>
-    <p>Pokémon es una franquicia de medios creada por Nintendo, Game Freak y Creatures. Fue introducida por primera vez en 1996 por Nintendo, Game Freak y Creatures para la consola Game Boy. La palabra "Pokémon" es una romanización de la marca japonesa Pocket Monsters (ポケットモンスター), que significa "monstruos de bolsillo".</p>
+    <div className='flex justify-center items-center flex-col gap-4 m-10' id='Que-es' >
+    <article className='flex flex-col gap-3 max-w-[900px]' >
+    <h3 className='text-xl font-semibold'>¿QUE ES POKEMON?</h3>
+    <p>Pokémon es una franquicia de medios creada por Nintendo, Game Freak y Creatures. Fue introducida por primera vez en 1996 por Nintendo, Game Freak y Creatures para la consola Game Boy. La palabra Pokémon es una romanización de la marca japonesa Pocket Monsters (ポケットモンスター), que significa monstruos de bolsillo.</p>
     <p>
     La franquicia Pokémon abarca videojuegos, series de televisión, películas, juegos de cartas coleccionables, juguetes y otros productos relacionados. La premisa central de Pokémon involucra a entrenadores, que son personajes ficticios, que capturan y entrenan criaturas llamadas Pokémon para batallar entre sí. Los Pokémon tienen habilidades diversas y evolucionan a formas más fuertes a medida que ganan experiencia en las batallas.    
     </p>
     </article>
     <picture>
-    <img
-    src='/Images/hero-imagen.png'
-    alt="Pokemon image" 
+    <Image
+    src={Pokemon}
+    width={300}
+    height={300}
+    alt='Pokemon image' 
     />
     </picture>
 
--- src/app/Componentes/Nav.js
+++ src/app/Componentes/Nav.js
@@ -1,6 +1,8 @@
 'use client'
+import pokebola from '../../../public/Images/pokeball.png'
 import Link from 'next/link'
 import { useState } from 'react'
+import Image from 'next/image'
 export default function Nav (){
     const [nav , setNav] = useState(false)
     const handleClick = () => {
@@ -12,7 +14,12 @@ export default function Nav (){
       <header className='flex justify-between w-screen h-20   items-center   fixed z-40 bg-blue-500'>
         <nav className='flex md:justify-evenly w-screen items-center'>
         <div className='text-black flex gap-4 text-lg md:text-xl  font-semibold z-50 ml-4'>
-        <img src="/Images/pokeball.png" alt="pokebola" className='w-8 object-cover'  />
+          <Image 
+          src={pokebola}
+          width={32}
+          height={32}
+          alt="pokebola"
+          />
         <Link href="/" className=' z-50' >POKE FOO</Link>
         </div>
         <ul className={`flex flex-col font-extrabold justify-center  items-center absolute bg-blue-500 w-screen h-screen  z-50 gap-4 text-xl md:text-xs  transition-all duration-300 md:flex-row md:relative md:w-fit md:h-fit ${nav ? "closeNav" : "openNav"}`}>
